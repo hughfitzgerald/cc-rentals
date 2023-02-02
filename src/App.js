5635aaf677405608af520cf32ac8ff8b4ec01813
hughfitzgerald <immobilesteelrims@gmail.com>
2023-02-02 15:08:59 -0800

    figuring out how to make a react app...

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect, useState } from "react";
+
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaHVnaGZpdHpnZXJhbGQiLCJhIjoiY2xkZGhjaG9wMDNqdTNvdDZ5bG80OXZ3YSJ9.6njmVnJyl0zAtMnM9d8duQ";
+
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-118.385796);
+  const [lat, setLat] = useState(33.97883);
+  const [zoom, setZoom] = useState(10.7);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/hughfitzgerald/cldjdvxl7000001qqfr6kpnpv",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
