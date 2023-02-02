e0f97d1f756d5c6601cb80495ce1ed6f9133b6d4
hughfitzgerald <immobilesteelrims@gmail.com>
2023-02-02 15:39:36 -0800

    basic react app

--- src/App.js
+++ src/App.js
@@ -22,8 +22,20 @@ export default function App() {
     });
   });
 
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
     <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
       <div ref={mapContainer} className="map-container" />
     </div>
   );
